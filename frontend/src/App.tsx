192240dfe1e1a8a8b5a150b7ceb4b6c2e8b7dd1d
Marcus Pang Yu Yang <marcuspangyuyang@gmail.com>
2024-11-16 16:30:33 +0700

    feat: add frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,62 @@
+import { flowTestnet } from "viem/chains";
+import {
+  createConfig,
+  http,
+  useAccount,
+  useConnect,
+  useDisconnect,
+} from "wagmi";
+import { injected } from "wagmi/connectors";
+
+export const config = createConfig({
+  chains: [flowTestnet],
+  connectors: [injected()],
+  transports: {
+    [flowTestnet.id]: http(),
+  },
+});
+
+function App() {
+  const account = useAccount();
+  const { connectors, connect, status, error } = useConnect();
+  const { disconnect } = useDisconnect();
+
+  return (
+    <>
+      <div>
+        <h2>Account</h2>
+
+        <div>
+          status: {account.status}
+          <br />
+          addresses: {JSON.stringify(account.addresses)}
+          <br />
+          chainId: {account.chainId}
+        </div>
+
+        {account.status === "connected" && (
+          <button type="button" onClick={() => disconnect()}>
+            Disconnect
+          </button>
+        )}
+      </div>
+
+      <div>
+        <h2>Connect</h2>
+        {connectors.map((connector) => (
+          <button
+            key={connector.uid}
+            onClick={() => connect({ connector })}
+            type="button"
+          >
+            {connector.name}
+          </button>
+        ))}
+        <div>{status}</div>
+        <div>{error?.message}</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
